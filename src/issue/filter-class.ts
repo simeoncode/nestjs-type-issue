f5793c674038da648e277a52dd2c69652183f862
Simeon <1130096+simeoncode@users.noreply.github.com>
2023-12-19 13:34:49 +0000

    Update filter-class.ts

--- src/issue/filter-class.ts
+++ src/issue/filter-class.ts
@@ -2,11 +2,9 @@ import { GraphQLScalarType, GraphQLString } from 'graphql';
 import { Type } from '@nestjs/common';
 import { Field } from '@nestjs/graphql';
 
-export function createFilterClass<T extends typeof GraphQLScalarType>(
-  FilterType: Type<T>,
-) {
+export function createFilterClass<T extends GraphQLScalarType>(FilterType: T) {
   class FilterComparators {
-    @Field(() => FilterType, { nullable: true })
+    @Field(() => FilterType.constructor as Type<T>, { nullable: true })
     eq?: T;
   }
   return FilterComparators;
